f8b53ed29446d6a0044f74d8564804127c2525e2
Daniel Filho <daniel.filho@forseti.com.br>
2019-05-06 18:03:43 -0300

    login

---
+++ login.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- <meta name="description" content="">
+    <meta name="author" content=""> -->
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>::Colégio Apolo::</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin" method="post" action="panel/login.php">
+      <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal"><img src="imagens/logo.png"></h1>
+      <label for="inputEmail" class="sr-only">E-mail</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" name="email" required autofocus>
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Senha" name="pass" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="yes" name="remember"> Mater conectado  - <a href="index.php" >Página inicial</a>
+        </label>
+      </div>
+        <button class="btn btn-lg btn-outline-light btn-block" type="submit">Acessar</button>
+      <p class="mt-5 mb-3 ">&copy; Colegio Apollo 2018 </br>Todos os direitos reservados </p>
+    </form>
+  </body>
+</html>
